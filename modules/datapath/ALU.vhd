b2e025aad79d296099f473b4b5c692f5d1146dcd
Alejandro Corona <alejocballester@gmail.com>
2026-04-28 22:33:01 +0200

    ALU implementation

--- modules/datapath/ALU.vhd
+++ modules/datapath/ALU.vhd
@@ -1,43 +1,32 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/25/2026 09:48:35 PM
--- Design Name: 
--- Module Name: ALU - Structural
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity ALU is
---  Port ( );
+    Port ( 
+            A       :   in  std_logic_vector (7 downto 0); -- Acumulador 
+            B       :   in  std_logic_vector (7 downto 0); -- Valor de memoria / inmediato
+            ALU_Sel :   in  std_logic_vector (1 downto 0); -- Código de Selección de instrucción
+            Result  :   out std_logic_vector (7 downto 0);
+            Zero    :   out std_logic -- Flag útil para BEQ, se activa cuando Result = 0
+            );
 end ALU;
 
-architecture Structural of ALU is
+architecture DataFlow of ALU is
+
+signal res_internal : std_logic_vector (7 downto 0);
 
 begin
 
+    with ALU_Sel select
+        res_internal <= 
+            std_logic_vector (unsigned(A) + unsigned(B)) when "000",
+            std_logic_vector (unsigned(A) - unsigned(B)) when "001",
+            (A AND B) when "010",
+            (A OR B) when "011";
+            
+    result <= res_internal;
+    
+    Zero <= '1' when (res_internal = "00000000") else '0';
 
-end Structural;
+end DataFlow;
