23b12b2f4165e09f51de590366144d8dbb6fb6e8
Mauricio Pimienta <mauriciopimienta.f@gmail.com>
2026-04-30 12:15:17 +0200

    Implemented ram_memory module

--- modules/io_memory/ram_data.vhd
+++ modules/io_memory/ram_data.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -32,12 +32,33 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity ram_data is
---  Port ( );
+    Port ( 
+        write_en        :   in std_logic ;
+        data_in         :   in std_logic_vector (7 downto 0);
+        data_out        :   out std_logic_vector (7 downto 0);
+        data_addr       :   in std_logic_vector (7 downto 0);
+        clk             :   in std_logic 
+    );
 end ram_data;
 
-architecture Structural of ram_data is
+architecture Behavioral of ram_data is
+
+    type ram_array is array (0 to 127) of STD_LOGIC_VECTOR(7 downto 0);
+    
+    signal ram : ram_array := (others => x"00");
 
 begin
 
+    process(clk)
+    begin 
+        if rising_edge(clk) then
+            if write_en = '1' then
+                ram(to_integer(unsigned(data_addr(6 downto 0)))) <= data_in;
+            end if;
+        end if;
+    end process;
+    
+    data_out <= ram(to_integer(unsigned(data_addr(6 downto 0))));
 
-end Structural;
+
+end Behavioral;
