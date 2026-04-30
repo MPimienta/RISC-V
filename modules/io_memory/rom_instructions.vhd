806aa0342b6619a086ca9ecbca8fbd936263ad6d
Mauricio Pimienta <mauriciopimienta.f@gmail.com>
2026-04-30 11:54:39 +0200

    First implementation of the ROM memory

--- modules/io_memory/rom_instructions.vhd
+++ modules/io_memory/rom_instructions.vhd
@@ -4,7 +4,7 @@
 -- 
 -- Create Date: 04/25/2026 09:48:35 PM
 -- Design Name: 
--- Module Name: rom_instructions - Structural
+-- Module Name: rom_instructions - DataFlow
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -32,12 +32,33 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity rom_instructions is
---  Port ( );
+    Port (
+        instruction_out     :   out std_logic_vector(7 downto 0);
+        instruction_addr    :   in std_logic_vector (7 downto 0)   
+    );
 end rom_instructions;
 
-architecture Structural of rom_instructions is
+
+architecture DataFlow of rom_instructions is
+
+    type instruction_array is array (0 to 255) of STD_LOGIC_VECTOR(7 downto 0); -- 256 espacios de 8 bits
+    
+    -- De momento se deja hardcodeado, pero hay que investigar cómo cargar un programa en memoria.
+    constant rom_memory   :   instruction_array   := (
+        0 => x"42", 
+        1 => x"05",
+
+        2 => x"44",
+        3 => x"03",
+        
+        4 => x"03",
+        5 => x"50",
+
+        others => x"00");
 
 begin
 
+    instruction_out <= rom_memory(to_integer(unsigned(instruction_addr)));
 
-end Structural;
+
+end DataFlow;
