12fe61704c74463f212df3b71cf63e9ae6abfec1
Mauricio Pimienta Fernández <mauriciopimienta.f@gmail.com>
2026-05-03 00:00:11 +0200

    Implemented decoder module

--- modules/control/decoder.vhd
+++ modules/control/decoder.vhd
@@ -4,7 +4,7 @@
 -- 
 -- Create Date: 04/25/2026 09:48:35 PM
 -- Design Name: 
--- Module Name: decoder - Structural
+-- Module Name: decoder - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -32,12 +32,67 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity decoder is
---  Port ( );
+    Port ( 
+        clk          : in std_logic;
+        reset        : in std_logic;
+        -- Conexiones hacia la CPU
+        cpu_addr     : in std_logic_vector(7 downto 0);
+        cpu_data_in  : in std_logic_vector(7 downto 0); 
+        cpu_mem_write: in std_logic;
+        cpu_data_out : out std_logic_vector(7 downto 0);
+        -- Conexiones hacia la RAM
+        ram_data_out : in std_logic_vector(7 downto 0); 
+        ram_we       : out std_logic;
+        -- Conexiones hacia el mundo físico
+        switches_in  : in std_logic_vector(7 downto 0);
+        buttons_in   : in std_logic_vector(7 downto 0);
+        leds_out     : out std_logic_vector(7 downto 0);
+        display_out  : out std_logic_vector(7 downto 0)
+    );
 end decoder;
 
-architecture Structural of decoder is
+architecture Behavioral of decoder is
 
+    signal leds    : std_logic_vector(7 downto 0) := (others => '0');
+    signal display : std_logic_vector(7 downto 0) := (others => '0');
+    
 begin
 
+    process(clk, reset)
+    begin
+        if reset = '1' then
+            leds <= (others => '0');
+            display <= (others => '0');
+            
+        elsif rising_edge(clk) then
+            ram_we <= '0'; 
 
-end Structural;
+            if cpu_mem_write = '1' then
+                if unsigned(cpu_addr) < 128 then
+                    ram_we <= '1';
+                elsif cpu_addr = x"F0" then
+                    leds <= cpu_data_in;
+                elsif cpu_addr = x"F1" then
+                    display <= cpu_data_in;
+                end if;
+            end if;
+        end if;
+    end process;
+    
+    leds_out <= leds;
+    display_out <= display;
+    
+    process(cpu_addr, ram_data_out, switches_in, buttons_in)
+    begin
+        if unsigned(cpu_addr) < 128 then
+            cpu_data_out <= ram_data_out;
+        elsif cpu_addr = x"E0" then
+            cpu_data_out <= switches_in;
+        elsif cpu_addr = x"E1" then
+            cpu_data_out <= buttons_in;
+        else
+            cpu_data_out <= (others => '0');
+        end if;
+    end process;
+
+end Behavioral;
