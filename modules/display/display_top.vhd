7c453169aaca0373fb3e5b967223cb5d9054db26
ArrobaAlex <al.rosado@alumnos.upm.es>
2026-05-10 02:45:23 +0200

    top display modules and top level of the whole system

---
+++ modules/display/display_top.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10.05.2026 02:37:09
+-- Design Name: 
+-- Module Name: display_top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity display_top is
+--  Port ( );
+end display_top;
+
+architecture Behavioral of display_top is
+
+begin
+
+
+end Behavioral;
